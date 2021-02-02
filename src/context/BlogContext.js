3bacf065dbeb54af95b0bd76ef748ad8c8f10022
Kole Gasior <jkole822@gmail.com>
2021-02-02 17:43:22 -0500

    edit actions in BlogContext; add useEffect in IndexScreen

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -11,15 +11,6 @@ const blogReducer = (state, action) => {
 			});
 		case "delete_blogpost":
 			return state.filter(blogPost => blogPost.id !== action.payload);
-		case "add_blogpost":
-			return [
-				...state,
-				{
-					id: Math.floor(Math.random() * 99999),
-					title: action.payload.title,
-					content: action.payload.content,
-				},
-			];
 		default:
 			return state;
 	}
@@ -33,20 +24,22 @@ const getBlogPosts = dispatch => {
 	};
 };
 
-const addBlogPost = dispatch => {
-	return (title, content, cb) => {
-		dispatch({ type: "add_blogpost", payload: { title, content } });
+const addBlogPost = () => {
+	return async (title, content, cb) => {
+		await jsonServer.post("/blogposts", { title, content });
 		cb();
 	};
 };
 
 const deleteBlogPost = dispatch => {
-	return id => {
+	return async id => {
+		await jsonServer.delete(`/blogposts/${id}`);
 		dispatch({ type: "delete_blogpost", payload: id });
 	};
 };
 const editBlogPost = dispatch => {
-	return (id, title, content, cb) => {
+	return async (id, title, content, cb) => {
+		await jsonServer.put(`/blogposts/${id}`, { title, content });
 		dispatch({ type: "edit_blogpost", payload: { id, title, content } });
 		cb();
 	};
